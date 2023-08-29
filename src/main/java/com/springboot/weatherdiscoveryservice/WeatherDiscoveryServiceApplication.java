41d48da30e0ecf3e497ecdb4ee3aa8e30468701e
praddesh <pradeep.acky@gmail.com>
2023-08-29 23:19:10 +0530

    updated discovery code

--- src/main/java/com/springboot/weatherdiscoveryservice/WeatherDiscoveryServiceApplication.java
+++ src/main/java/com/springboot/weatherdiscoveryservice/WeatherDiscoveryServiceApplication.java
@@ -2,8 +2,10 @@ package com.springboot.weatherdiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WeatherDiscoveryServiceApplication {
 
 	public static void main(String[] args) {
